e6bdd7a6ea1cc031500ae8e11768b5f40c2a49f7
Luis Anaya <brothersailorwebdev@gmail.com>
2023-09-13 20:14:56 +0200

    fixing prev

--- lib/get-random-media.tsx
+++ lib/get-random-media.tsx
@@ -1,32 +1,32 @@
-// // const prevMediaObj = {
-// //   prev: 1,
-// //   setPrev: function (num: number) {
-// //     this.prev = num
-// //   },
-// // }
-
-// // export const getRandomMedia = async (data: any) => {
-// //   let randomIndex = prevMediaObj.prev
-
-// //   while (randomIndex === prevMediaObj.prev) {
-// //     randomIndex = Math.floor(Math.random() * data.length)
-// //   }
-
-// //   prevMediaObj.setPrev(randomIndex)
-
-// //   return data[randomIndex]
-// // }
-
-export const getRandomMedia = async (array: any) => {
-  let usedIndexArray = [] as number[]
-  const max = array.length
-  let randomIndex
-
-  do {
-    randomIndex = Math.floor(Math.random() * max)
-  } while (usedIndexArray.includes(randomIndex))
-
-  usedIndexArray.push(randomIndex)
-
-  return array[randomIndex]
+const prevMediaObj = {
+  prev: 1,
+  setPrev: function (num: number) {
+    this.prev = num
+  },
 }
+
+export const getRandomMedia = async (data: any) => {
+  let randomIndex = prevMediaObj.prev
+
+  while (randomIndex === prevMediaObj.prev) {
+    randomIndex = Math.floor(Math.random() * data.length)
+  }
+
+  prevMediaObj.setPrev(randomIndex)
+
+  return data[randomIndex]
+}
+
+// // export const getRandomMedia = async (array: any) => {
+// //   let usedIndexArray = [] as number[]
+// //   const max = array.length
+// //   let randomIndex
+
+// //   do {
+// //     randomIndex = Math.floor(Math.random() * max)
+// //   } while (usedIndexArray.includes(randomIndex))
+
+// //   usedIndexArray.push(randomIndex)
+
+// //   return array[randomIndex]
+// // }
