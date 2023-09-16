291f2c81e88448934dd06879f8345b8e207363f7
Luis Anaya <brothersailorwebdev@gmail.com>
2023-09-16 16:04:49 +0200

    added next ui

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,16 @@ export default async function HomeRoute() {
         Cover Guessr
       </h1>
       <p className="prose md:prose-xl mt-2.5 max-w-prose text-center">
+        Guess the release year of an album, movie, or videogame.
+      </p>
+      {/* <p className="prose md:prose-xl mt-2.5 max-w-prose text-center">
         Guess the release year of an album, movie, or videogame. If you want to
         save your score history, log in first. Otherwise, you can play as a
         guest.
-      </p>
-      <div className="mt-2.5 flex items-center justify-center gap-x-5">
+      </p> */}
+      {/* <div className="mt-2.5 flex items-center justify-center gap-x-5">
         <AuthButtonServer />
-      </div>
+      </div> */}
 
       <section className="mt-10 flex flex-col items-center justify-center">
         <p>Start a game:</p>
