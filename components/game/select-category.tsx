b49161de4ed7f13ebba8f15989d86c9457867014
Luis Anaya <brothersailorwebdev@gmail.com>
2023-09-21 13:50:54 +0200

    fixed select category gap

--- components/game/select-category.tsx
+++ components/game/select-category.tsx
@@ -9,16 +9,19 @@ import { SelectButton } from "@/components/game/select-button"
 
 export const SelectCategory = () => {
   return (
-    <div className="mt-2.5 flex gap-x-5">
+    <div className="mt-5 grid grid-cols-4 gap-x-5">
       <SelectButton icon={<ShapesIcon className="h-4 w-4" />} category="all" />
       <SelectButton
         icon={<ClapperboardIcon className="h-4 w-4" />}
-        category="film"
+        category="movies"
       />
-      <SelectButton icon={<Disc3Icon className="h-4 w-4" />} category="music" />
+      <SelectButton
+        icon={<Disc3Icon className="h-4 w-4" />}
+        category="albums"
+      />
       <SelectButton
         icon={<Gamepad2Icon className="h-4 w-4" />}
-        category="games"
+        category="videogames"
       />
     </div>
   )
