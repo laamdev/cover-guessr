fd507cc88725458c7dfd8c4d3c3fbe4652699361
Luis Anaya <brothersailorwebdev@gmail.com>
2023-09-21 13:54:04 +0200

    wrapped select category with link

--- components/game/select-button.tsx
+++ components/game/select-button.tsx
@@ -17,16 +17,19 @@ export const SelectButton = ({
   const param = category === "all" ? "" : category
 
   return (
-    <div className="flex flex-col items-center gap-y-1">
+    <Link
+      href={`/game?category=${param}`}
+      className="flex flex-col items-center gap-y-1"
+    >
       <Button
         isIconOnly
         size="sm"
         aria-label={category}
         onClick={() => gameStore.reset()}
       >
-        <Link href={`/game?category=${param}`}>{icon}</Link>
+        {icon}
       </Button>
       <p className="text-xs uppercase">{category}</p>
-    </div>
+    </Link>
   )
 }
